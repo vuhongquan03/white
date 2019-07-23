63f33a9503cd46616c4a2b7ed86896b7101515b4
tanptn <tanptn@d2t.vn>
2019-07-23 13:49:16 +0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, ViewChild} from '@angular/core';
+import {MdbTableDirective} from "PATH-TO-MDB-ANGULAR";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'white';
-}
+
+    @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
+    elements: any = [];
+    headElements = ['ID', 'First', 'Last', 'Handle'];
+  
+    searchText: string = '';
+    previous: string;
+  
+    constructor() { }
+  
+    @HostListener('input') oninput() {
+      this.searchItems();
+    }
+  
+    ngOnInit() {
+      for (let i = 1; i <= 10; i++) {
+        this.elements.push({ id: i.toString(), first: 'Wpis ' + i, last: 'Last ' + i, handle: 'Handle ' + i });
+      }
+  
+      this.mdbTable.setDataSource(this.elements);
+      this.elements = this.mdbTable.getDataSource();
+      this.previous = this.mdbTable.getDataSource();
+    }
+  
+    searchItems() {
+      const prev = this.mdbTable.getDataSource();
+  
+      if (!this.searchText) {
+        this.mdbTable.setDataSource(this.previous);
+        this.elements = this.mdbTable.getDataSource();
+      }
+  
+      if (this.searchText) {
+        this.elements = this.mdbTable.searchLocalDataBy(this.searchText);
+        this.mdbTable.setDataSource(prev);
+      }
+    }
+  }
